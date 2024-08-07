92e5d8631c24c5b4b55e56bf8e073235ab5a61bc
Grazielle Nascimento <grazielle.nascimento0724@jala.university>
2024-08-07 20:04:52 -0300

    chore: Refactor proof.routes.js to use courseProof router

--- src/routes/proof.routes.js
+++ src/routes/proof.routes.js
@@ -1,19 +1,11 @@
 import express from "express";
+import { createProof, deleteProof, getAllProofs, updateProof } from "../controllers/proof.controllers.js";
 
-const proofRouter = express.Router();
+const courseProof = express.Router();
 
+courseProof.post('/proof', createProof);
+courseProof.get('/proof', getAllProofs);
+courseProof.delete('/proof/:id', deleteProof);
+courseProof.put('/proof/:id', updateProof);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default proofRouter;
+export default courseProof;
