bac18acc25bd694a1d13c0bdfee825a7f5ffe84a
VictorCDS-p <Victor90774@outlook.com>
2024-08-08 21:33:31 -0300

    add post, get, put and delete in routes

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -2,10 +2,10 @@ import express from "express";
 import { createStudent, getAllStudents, updateStudent, deleteStudent } from "../controllers/student.controllers.js";
 const studentRouter = express.Router();
 
-studentRouter("/student", createStudent)
-studentRouter("/student", getAllStudents)
-studentRouter("/student/:registration", updateStudent)
-studentRouter("/student/:registration", deleteStudent)
+studentRouter.post("/student", createStudent)
+studentRouter.get("/student", getAllStudents)
+studentRouter.put("/student/:registration", updateStudent)
+studentRouter.delete("/student/:registration", deleteStudent)
 
 
 export default studentRouter;
